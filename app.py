e47117376d3645381047dba7086abe9cf2d050e0
OlegKuro <o.khayatov@skyeng.ru>
2022-01-07 21:00:01 +0300

    typehints

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 
-from flask import Flask, request
-from .helpers import compose_closures, get_closure
+from flask import Flask, request, Response
+from .helpers import get_response
+from typing import Tuple, Union
 
 app = Flask(__name__)
 
@@ -9,21 +10,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
 
-def read_file(file_name):
-    for row in open(f'data/{file_name}', 'r'):
-        yield row
-
-
-def get_response(params: dict):
-    logs = read_file(params['file_name'])
-    return compose_closures(
-        get_closure(params['cmd1'], params['value1']),
-        get_closure(params['cmd2'], params['value2'])
-    )(logs)
-
-
 @app.route("/perform_query", methods=('POST',))
-def perform_query():
+def perform_query() -> Union[Tuple[str, int], Response]:
     params = request.form.to_dict()
     if len({'value1', 'value2', 'cmd1', 'cmd2', 'file_name'} & set(params.keys())) != 5:
         return 'Args validation error', 400
@@ -32,4 +20,4 @@ def perform_query():
         return app.response_class(ret, content_type='text/plain')
     except Exception as e:
         print(e)
-        return 'Some exception occured T_T', 500
+        return 'Some exception occurred T_T', 500
--- helpers.py
+++ helpers.py
@@ -1,43 +1,70 @@
 import itertools
+import re
 from functools import reduce
+from typing import Dict, Callable, Iterable, Generator
 
 
-def make_filter(filter_phrase):
-    def filterer(logs):
+def get_response(params: Dict[str, str]) -> Iterable[str]:
+    logs = read_file(params['file_name'])
+    return compose_closures(
+        get_closure(params['cmd1'], params['value1']),
+        get_closure(params['cmd2'], params['value2'])
+    )(logs)
+
+
+def read_file(file_name: str) -> Generator[str, None, None]:
+    for row in open(f'data/{file_name}', 'r'):
+        yield row
+
+
+def make_filter(filter_phrase: str) -> Callable[[Iterable[str]], Iterable[str]]:
+    def filterer(logs: Iterable[str]) -> Iterable[str]:
         return filter(lambda logs_line: filter_phrase in logs_line, logs)
 
     return filterer
 
 
-def make_map(idx):
-    def mapper(logs):
+def make_map(idx: int) -> Callable[[Iterable[str]], Iterable[str]]:
+    def mapper(logs: Iterable[str]) -> Iterable[str]:
         return map(lambda logs_line: logs_line.split(' ')[idx], logs)
 
     return mapper
 
 
-def make_unique():
-    def unique(logs):
-        return logs
+def make_unique() -> Callable[[Iterable[str]], Iterable[str]]:
+    def unique(logs: Iterable[str]) -> Iterable[str]:
+        return set(logs)
 
     return unique
 
 
-def make_sort(is_asc=True):
-    def sort(logs):
+def make_sort(is_asc: bool = True) -> Callable[[Iterable[str]], Iterable[str]]:
+    def sort(logs: Iterable[str]) -> Iterable[str]:
         return sorted(logs, reverse=not is_asc)
 
     return sort
 
 
-def make_limit(count):
-    def limit(logs):
+def make_limit(count: int) -> Callable[[Iterable[str]], Iterable[str]]:
+    def limit(logs: Iterable[str]) -> Iterable[str]:
         return itertools.islice(logs, 0, count)
 
     return limit
 
 
-def get_closure(type: str, param):
+def make_regex(r_str: str) -> Callable[[Iterable[str]], Iterable[str]]:
+    pattern = re.compile(r_str)
+
+    def regex(logs: Iterable[str]) -> Iterable[str]:
+        return filter(
+            lambda logs_line: bool(re.search(pattern, logs_line)),
+            logs
+        )
+
+    return regex
+
+
+def get_closure(type: str, param: str) -> Callable[[Iterable[str]], Iterable[str]]:
     if type == 'filter':
         return make_filter(param)
     if type == 'map':
@@ -48,10 +75,13 @@ def get_closure(type: str, param):
         return make_sort(param == 'asc')
     if type == 'limit':
         return make_limit(int(param))
+    if type == 'regex':
+        return make_regex(param)
+    return lambda x: x
 
 
-def compose_closures(*closures):
-    def inner(f, g):
+def compose_closures(*closures: Callable) -> Callable:
+    def inner(f: Callable, g: Callable) -> Callable:
         return lambda *args, **kwargs: f(g(*args, **kwargs))
 
     return reduce(inner, reversed(closures), lambda x: x)
