549481efa3e062870fe5075f537f795e9e27ec33
OlegKuro <o.khayatov@skyeng.ru>
2022-01-09 00:04:53 +0300

    typehints review fix

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import os
 
 from flask import Flask, request, Response
 from .helpers import get_response
-from typing import Tuple, Union
 
 app = Flask(__name__)
 
@@ -11,13 +10,23 @@ DATA_DIR = os.path.join(BASE_DIR, "data")
 
 
 @app.route("/perform_query", methods=('POST',))
-def perform_query() -> Union[Tuple[str, int], Response]:
+def perform_query() -> Response:
     params = request.form.to_dict()
     if len({'value1', 'value2', 'cmd1', 'cmd2', 'file_name'} & set(params.keys())) != 5:
-        return 'Args validation error', 400
+        return app.response_class(
+            response='Args validation error',
+            status=400,
+            content_type='text/plain'
+        )
     try:
-        ret = get_response(params)
-        return app.response_class(ret, content_type='text/plain')
+        return app.response_class(
+            response=get_response(params),
+            content_type='text/plain'
+        )
     except Exception as e:
         print(e)
-        return 'Some exception occurred T_T', 500
+        return app.response_class(
+            response='Some exception occurred T_T',
+            status=500,
+            content_type='text/plain'
+        )
